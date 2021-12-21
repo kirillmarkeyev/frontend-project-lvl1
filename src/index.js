fe176b3dcd9d07603aafbacb6f17a248f6cd6121
kirillmarkeyev <kirillmarkeyev@mail.ru>
2021-12-21 21:34:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+export const userWelcome = () => {
+  console.log('Welcome to the Brain Games!');
+};
+
+export const userGreeting = (userName) => `Hello, ${userName}!`;
+
+export const userCongratulate = (userName) => `Congratulations, ${userName}!`;
+
 export const gameDescription = (description) => description;
 
 export const getRandomInRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
