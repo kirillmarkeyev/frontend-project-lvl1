de620527586acd1182e0a1a6fcb5bd15cdf8ebbb
kirillmarkeyev <kirillmarkeyev@mail.ru>
2021-12-21 22:33:30 +0300

    Fix calc.js

--- src/games/calc.js
+++ src/games/calc.js
@@ -36,7 +36,7 @@ export default () => {
     if (userAnswer === rightAnswer) {
       console.log('Correct!');
     } else {
-      console.log(`${userAnswer} is wrong answer ;(. Correct answer was ${rightAnswer}.\nLet's try again, ${userName}!`);
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${userName}!`);
       return;
     }
   } userCongratulate(userName);
