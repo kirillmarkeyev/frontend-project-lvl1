c056f7be5f3078284c7658b9545dee8ad4b60403
kirillmarkeyev <kirillmarkeyev@mail.ru>
2021-12-21 22:33:06 +0300

    Fix index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ export const userWelcome = () => {
   console.log('Welcome to the Brain Games!');
 };
 
-export const userGreeting = (userName) => `Hello, ${userName}!`;
+export const userGreeting = (userName) => {
+  console.log(`Hello, ${userName}!`);
+};
 
-export const userCongratulate = (userName) => `Congratulations, ${userName}!`;
+export const userCongratulate = (userName) => {
+  console.log(`Congratulations, ${userName}!`);
+};
 
-export const gameDescription = (description) => description;
+export const gameDescription = (description) => {
+  console.log(description);
+};
 
 export const getRandomInRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
