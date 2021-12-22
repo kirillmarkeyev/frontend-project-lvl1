e5e93243837bf2ad2c3afcaf705c2d84edb6f9f6
kirillmarkeyev <kirillmarkeyev@mail.ru>
2021-12-22 17:03:42 +0300

    Add gcd.js

---
+++ src/games/gcd.js
@@ -0,0 +1,44 @@
+import readlineSync from 'readline-sync';
+
+import {
+  userWelcome, gameDescription, getRandomInRange, userCongratulate, userGreeting,
+} from '../index.js';
+
+const gcd = (a, b) => {
+  if (a === b) {
+    return a;
+  }
+  if (a > b) {
+    if (b === 0) {
+      return a;
+    } return gcd(b, a % b);
+  }
+  if (b > a) {
+    if (a === 0) {
+      return b;
+    } return gcd(a, b % a);
+  } return a;
+};
+
+export default () => {
+  const description = 'Find the greatest common divisor of given numbers.';
+  const rounds = 3;
+  userWelcome();
+  const userName = readlineSync.question('May I have your name? ');
+  userGreeting(userName);
+  gameDescription(description);
+  for (let i = 0; i < rounds; i += 1) {
+    const firstNumber = getRandomInRange(1, 30);
+    const secondNumber = getRandomInRange(1, 30);
+    const rightAnswer = gcd(firstNumber, secondNumber);
+    console.log(`Question: ${firstNumber} ${secondNumber}`);
+    let userAnswer = readlineSync.question('Your answer: ');
+    userAnswer = Number(userAnswer);
+    if (userAnswer === rightAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${userName}!`);
+      return;
+    }
+  } userCongratulate(userName);
+};
