3b86f55490788b56d445aaa608eed3bce7eda1ad
kirillmarkeyev <kirillmarkeyev@mail.ru>
2021-12-22 23:42:15 +0300

    Add new const

--- src/index.js
+++ src/index.js
@@ -15,3 +15,5 @@ export const gameDescription = (description) => {
 };
 
 export const getRandomInRange = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+export const rounds = 3;
