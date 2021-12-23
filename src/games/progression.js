08f4aa12c2cc029d95c350e2bca4ed9a054a901a
kirillmarkeyev <kirillmarkeyev@mail.ru>
2021-12-23 15:35:57 +0300

    Add some comments

--- src/games/progression.js
+++ src/games/progression.js
@@ -11,18 +11,27 @@ export default () => {
   userGreeting(userName);
   gameDescription(description);
   for (let i = 0; i < rounds; i += 1) {
+    // Создание пустого массива
     const arr = [];
+    // Генерация длины массива
     const arrLength = getRandomInRange(5, 10);
+    // Генерация первого элемента прогрессии
     const firstElement = getRandomInRange(0, 10);
+    // Генерация шага прогрессии
     const progressionStep = getRandomInRange(2, 5);
+    // Добавление членов прогрессии в массив
     let currentElement = firstElement;
     for (let j = 0; j < arrLength; j += 1) {
       arr.push(currentElement);
       currentElement += progressionStep;
     }
+    // Определение позиции пропущенного элемента
     const missingPlace = getRandomInRange(0, arrLength - 1);
+    // Запись значения пропущенного элемента в переменную
     const rightAnswer = arr[missingPlace];
+    // Замена числа на '..'
     arr[missingPlace] = '..';
+    // Трансформация массива в строку
     const progression = arr.join(' ');
     console.log(`Question: ${progression}`);
     let userAnswer = readlineSync.question('Your answer: ');
