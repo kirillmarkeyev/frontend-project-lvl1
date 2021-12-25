accd0d0d93ea080ef5b45c10104bace2d9a6d5d8
kirillmarkeyev <kirillmarkeyev@mail.ru>
2021-12-25 18:53:35 +0300

    Upgrade even.js after code-review 24.12.2021

--- src/games/even.js
+++ src/games/even.js
@@ -1,25 +1,16 @@
-import readlineSync from 'readline-sync';
-
 import {
-  userWelcome, gameDescription, rounds, getRandomInRange, userCongratulate, userGreeting,
+  getRandomInRange, commonGameLogic,
 } from '../index.js';
 
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const roundGenerator = () => {
+  const question = getRandomInRange(1, 100);
+  const rightAnswer = question % 2 === 0 ? 'yes' : 'no';
+  // Используем массив, чтобы функция возвращала сразу два значения
+  return [question, rightAnswer];
+};
+
 export default () => {
-  const description = 'Answer "yes" if the number is even, otherwise answer "no".';
-  userWelcome();
-  const userName = readlineSync.question('May I have your name? ');
-  userGreeting(userName);
-  gameDescription(description);
-  for (let i = 0; i < rounds; i += 1) {
-    const randomNumber = getRandomInRange(1, 100);
-    const rightAnswer = randomNumber % 2 === 0 ? 'yes' : 'no';
-    console.log(`Question: ${randomNumber}`);
-    const userAnswer = readlineSync.question('Your answer: ');
-    if (userAnswer === rightAnswer) {
-      console.log('Correct!');
-    } else {
-      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${rightAnswer}'.\nLet's try again, ${userName}!`);
-      return;
-    }
-  } userCongratulate(userName);
+  commonGameLogic(description, roundGenerator);
 };
